81bcbf943be44b5934954837352d1c11fe2fc49c
Hassan360az <mhn360az@gmail.com>
2026-06-10 15:34:38 +0500

    List_practice

---
+++ list.py
@@ -0,0 +1,37 @@
+# new_users = ["ali", "ahmed", "usman"]
+
+# if new_users[0] == "ali":
+#     print("your code is 301")
+# else:
+#     print ("enter valid user name")
+
+# new_list = ["banana" , "cherry"]
+
+# print(new_list[-1])
+
+# new_list.append("orange")
+
+# print(new_list)
+
+# new_list = ["ali", "usama"]
+
+# for i in new_list:
+#     print(new_list[0])
+
+# random_fruites = ["Banana" , "cherry" , "Apple"]
+
+# random_fruites.sort()
+
+# print(random_fruites)
+
+#copy a list
+
+random_user = ["ali", 2]
+
+list_new = random_user.copy()
+
+print(list_new)
+
+join_list = random_user + list_new
+
+print (join_list)
