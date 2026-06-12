c8907ff60f28c175ba8eafa4d61d4bfcbc912045
Hassan360az <mhn360az@gmail.com>
2026-06-12 16:28:59 +0500

    sets practice

---
+++ sets.py
@@ -0,0 +1,15 @@
+my_set = {"true", "banana"}
+
+print(my_set)
+
+exampleset = {"apple", "banana", "apple"}
+
+for z in exampleset:
+  print(z)
+
+anotheset = {"one",  "two"}
+
+anotheset.add("three")
+
+print (anotheset)
+
