1ec38e5a505b29d635c472d97d205e71bc3c3bf0
Hassan360az <mhn360az@gmail.com>
2026-06-13 23:43:16 +0500

    Dictionary Practice

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+mydictionay = {
+    "name": "abc",
+    "age": 34,
+}
+
+print (mydictionay["name"])
